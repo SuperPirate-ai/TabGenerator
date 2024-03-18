faf01ef4506c4504379e9cc0cf495fa823cb5675
SuperPirate-ai <minjag@web.de>
2024-03-18 22:06:09 +0100

    logic for rearanging measureBars on BPM-change

--- Assets/Scripts/Tools/MeasureBarGenerator.cs
+++ Assets/Scripts/Tools/MeasureBarGenerator.cs
@@ -15,12 +15,13 @@ public class MeasureBarGenerator : MonoBehaviour
     private void Start()
     {
         Metronome.metronome.Elapsed += OnNewBeat;
+        EventManager.StartListening("BPMChanged", OnBPMChange);
     }
     private void Update()
     {
         if(instatiateMeasureBar)
         {
-            InstanciateNewMeasureBar();
+            CreateNewMeasureBar();
             instatiateMeasureBar = false;
         }
     }
@@ -34,18 +35,46 @@ public class MeasureBarGenerator : MonoBehaviour
             instatiateMeasureBar = true;
         }
     }
-    void InstanciateNewMeasureBar()
+    void CreateNewMeasureBar()
     {
         Debug.Log("Instantiating...");
-        try
+
+        GameObject go = Instantiate(MeasureBar.gameObject, Pointer.transform.position, Quaternion.identity);
+        NoteManager.Instance.MeasureBars.Add(go);
+    }
+    void InstanciateMeasureBar(GameObject _measureBar, Vector3 _position)
+    {
+        GameObject go = Instantiate(MeasureBar.gameObject, Pointer.transform.position, Quaternion.identity);
+        NoteManager.Instance.MeasureBars.Add(go);
+    }
+    void OnBPMChange(Dictionary<string, object> _message)
+    {
+        List<GameObject> measurebars = NoteManager.Instance.MeasureBars;
+        NoteManager.Instance.MeasureBars.Clear();
+        int oldBPM = NoteManager.Instance.BPM;
+        int newBPM = Convert.ToInt32(_message["BPM"]);
+        NoteManager.Instance.BPM = Convert.ToInt32(_message["BPM"]);
+                
+        float distanceBewtweenOLDBars = (Mathf.Abs(measurebars[0].transform.position.x) - Mathf.Abs(measurebars[1].transform.position.x));
+        float timeBetweenOLDBars = 60 / oldBPM;
+
+        float timeBetweenBars = 60 / newBPM;
+
+        var value = distanceBewtweenOLDBars / timeBetweenOLDBars;
+
+        float distanceBetweenNEWBars = value * timeBetweenBars;
+
+        foreach (var item in measurebars)
         {
-            GameObject go = Instantiate(MeasureBar.gameObject, Pointer.transform.position, Quaternion.identity);
-            NoteManager.Instance.MeasureBars.Add(go);
+            try
+            {
+                Destroy(item.gameObject);
+            }
+            catch(Exception e)
+            {
+                print(e);
+            }
         }
-        catch(Exception e) 
-        {
-            print(e);
-        }
-       
+        
     }
 }
